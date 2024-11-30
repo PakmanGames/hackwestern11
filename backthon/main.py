0ff64ab1ad56bca89d111544629c750ff80048ec
James Liang <86000097+jamesliangg@users.noreply.github.com>
2024-11-30 15:36:32 -0500

    Reformat GET response

--- backthon/main.py
+++ backthon/main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+list_of_articles = []
+current_index = 0
 
 class ScrapeRequest(BaseModel):
     url: str
@@ -17,12 +19,22 @@ def read_root():
 
 @app.get("/scrape")
 def scrape_info(request: ScrapeRequest):
+    global current_index
+    global list_of_articles
     url = request.url
     # print(url)
     article = NewsPlease.from_url(url)
-    print(article)
-    print(article.title)
-    print(article.description)
-    print(article.maintext)
-    print(article.image_url)
-    return {article}
+    new_article = {
+        "headline": article.title,
+        "content": article.maintext,
+        "image_caption": "bau bau bau",
+        "article_index": current_index
+    }
+    current_index += 1
+    # print(article)
+    # print(article.title)
+    # print(article.description)
+    # print(article.maintext)
+    # print(article.image_url)
+    list_of_articles.append(new_article)
+    return new_article
