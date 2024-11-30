c3aee955dd7186f7324791bebf6f1152657c9601
Andy Pak <im.andy.pak@gmail.com>
2024-11-30 10:13:37 -0500

    Create sidebar components

--- frontend/components/sideBar.js
+++ frontend/components/sideBar.js
@@ -1,12 +1,18 @@
 import SidebarTopArticle from "./sideBarTopArticle";
+import SidebarTinyArticle from "./sideBarTinyArticle";
+import SidebarLongArticle from "./sideBarLongArticle";
 
 export default function SideBar() {
     return (
         <div>
             <SidebarTopArticle />
-            <div>
-                Two smaller articles
+            <div className="flex justify-between p-4 border-t">
+                <SidebarTinyArticle />
+                <SidebarTinyArticle />
             </div>
+            <SidebarLongArticle />
+            <SidebarLongArticle />
+            <SidebarLongArticle />
         </div>
     );
 }
---
+++ frontend/components/sideBarLongArticle.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import temp from "@/public/tempImage.png";
+
+export default function SidebarLongArticle() {
+  return (
+    <div className="flex items-start gap-4 border-t p-4 pb-4 max-w-md">
+      <div className="flex-1">
+        <h3 className="text-lg font-bold leading-tight">
+          lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </h3>
+        <p className="text-xs text-gray-500 mt-1">2 MIN READ</p>
+      </div>
+      <div className="w-24 h-24 flex-shrink-0">
+        <Image
+          src={temp}
+          alt="Article Image"
+          className="w-full h-full object-cover rounded"
+        />
+      </div>
+    </div>
+  );
+}
