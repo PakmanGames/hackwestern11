6d35d5860469e2c1f8139d396266f53ec367b853
James Liang <86000097+jamesliangg@users.noreply.github.com>
2024-11-30 13:56:17 -0500

    FastAPI server

---
+++ backthon/README.md
@@ -0,0 +1,14 @@
+## Run server
+`fastapi dev main.py`
+
+## GET Request
+Endpoint
+```markdown
+http://127.0.0.1:8000/scrape
+```
+Body
+```markdown
+{
+    "url": "https://www.nytimes.com/2024/11/30/world/middleeast/syria-aleppo-rebels-control.html"
+}
+```
--- backthon/main.py
+++ backthon/main.py
@@ -1,6 +1,28 @@
 from newsplease import NewsPlease
-article = NewsPlease.from_url('https://www.cbc.ca/news/politics/trudeau-meets-trump-tariff-1.7397564')
-print(article.title)
-print(article.description)
-print(article.maintext)
-print(article.image_url)
+from typing import Union
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class ScrapeRequest(BaseModel):
+    url: str
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/scrape")
+def scrape_info(request: ScrapeRequest):
+    url = request.url
+    # print(url)
+    article = NewsPlease.from_url(url)
+    print(article)
+    print(article.title)
+    print(article.description)
+    print(article.maintext)
+    print(article.image_url)
+    return {article}
