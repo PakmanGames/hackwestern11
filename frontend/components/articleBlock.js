6b5c6a95955ca4f910a7481bdccbc3a8e71946f5
Andy Pak <im.andy.pak@gmail.com>
2024-11-30 21:09:42 -0500

    Make article pages more dynamic

--- frontend/components/articleBlock.js
+++ frontend/components/articleBlock.js
@@ -2,16 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import temp from "@/public/tempImage.png";
 
-export default function ArticleBlock() {
+export default function ArticleBlock({ info }) {
     return (
-        <Link href={`/news/1`}>
+        <Link href={`/news/${info.article_index}`}>
             <div className="flex flex-col md:flex-row items-start gap-6 border-b border-r p-4 pb-6 max-w-5xl">
                 <div className="md:w-2/3 space-y-2">
                     <h2 className="text-2xl font-bold leading-tight">
-                        lorem ipsum dolor sit amet, consectetur adipiscing elit
+                        {info.headline}
                     </h2>
                     <p className="text-sm text-gray-700">
-                        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        {info.description}
                     </p>
                     <p className="text-xs text-gray-500 mt-2">4 MIN READ</p>
                 </div>
